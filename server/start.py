7c638c176e4162536c5f5c7c2c59ee8cc2e8cfad
Ghulam Murtaza <Ghulam.murtaza@mail.mcgill.ca>
2024-11-17 03:10:20 -0500

    fix the port configuration

--- server/start.py
+++ server/start.py
@@ -3,4 +3,5 @@ import uvicorn
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", "8000"))
+    print(f"Starting server on port: {port}")
     uvicorn.run("api.main:app", host="0.0.0.0", port=port, reload=False)
